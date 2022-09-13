b7ed3ad474744c212f19203893d8313f809f25ea
icoo08217 <icoo08216@gmail.com>
2022-09-13 14:38:54 +0900

    테스트 환경의 샘플데이터 생성, 테스트 DB 분리

--- src/test/java/com/ll/exam/app10/AppTests.java
+++ src/test/java/com/ll/exam/app10/AppTests.java
@@ -1,15 +1,19 @@
 package com.ll.exam.app10;
 
+import com.ll.exam.app10.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,10 +22,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles({"base-addi" , "test"})
 public class AppTests {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private MemberService memberService;
 
     @Test
     @DisplayName("메인화면에서는 안녕이 나와야한다.")
@@ -36,5 +44,13 @@ public class AppTests {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().methodName("main"))
                 .andExpect(content().string(containsString("안녕")));
+    }
+
+    @Test
+    @DisplayName("회원의 수")
+    @Rollback(false)
+    void t2() throws Exception {
+        long count = memberService.count();
+        assertThat(count).isGreaterThan(0);
     }
 }
